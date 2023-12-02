d4c9471fe0d840d937604290dd124aaeb0494b97
John Heger <heger7@gmail.com>
2023-12-01 18:29:50 -0600

    completed lab

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+
+// Need to use a value prop for this new Search input field.  
+// Need to create a Search component?  
+// Where does state need to be added? I think on Filter.js, ItemForm.js, ShoppingList.js, and App.js
+// Set up intial state, connect that state to the input field.  Controlled input, so the input's value should always be in sync with the state
+// Find a way to set state when the input changes.  Use a prop called onSearchChange as a callback
+// use state value to determine which items are being displayed on the page, similar to the category dropdown
+
+
+function Filter({ onCategoryChange, onSearchChange, search }) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text" value={search} name="search" placeholder="Search..." onChange={onSearchChange} />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
